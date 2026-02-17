3b2527b9164d7d170f68e7c775b26c6e82787ad5
Kartik Bhoir <kartikbhoir2003@gmail.com>
2026-02-17 19:25:35 +0530

    few changes in css

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const input_wins = document.getElementById('win-input');
 const addbtn = document.getElementById('add-wins')
 const listContainer = document.querySelector('.win_list');
+/** change theme */
+const changetheme = document.getElementById('theme-changer').addEventListener('click', () =>{
 
+    let theme = document.body;
+    theme.classList.toggle('dark-mode');
+})
+/** restore on list on refresh */
 let storage_box = JSON.parse(localStorage.getItem('lists')) || [];
 
 storage_box.forEach(item => {
@@ -10,6 +16,7 @@ storage_box.forEach(item => {
     listContainer.appendChild(li);
 });
 
+/** input lists to localstorage */
 addbtn.addEventListener('click' , () => {
    const inputs = input_wins.value.trim();
 
